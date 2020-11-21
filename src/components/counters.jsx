e8bb395f308b7cfae16fe3690c4187f7378c5f69
Andrea-Dispe <dispe.andrea@gmail.com>
2020-11-21 08:10:59 +0800

    testing update from laptop and branching

---
+++ src/components/counters.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Counter from './counter';
+
+class Counters extends Component {
+  state = {  }
+  render() {
+    return (
+      <>
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      </>
+    );
+  }
+}
+
+export default Counters;
