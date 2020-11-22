6a62fa34f1bd8f80f33af1f823a10f233e6a82c2
Andrea-Dispe <dispe.andrea@gmail.com>
2020-11-22 11:48:19 +0800

    Feat: passing data from child to parent

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -16,17 +16,27 @@ green20 = {
 
 }
 
+handleIncrement = () => {
+  // remember the setState takes an object
+  console.log(this.props)
+  this.setState({value: this.state.value + 1})
+}
+
   render() {
     return (
       <>
         <span>This State value {this.state.value}</span>
+        <button onClick={this.handleIncrement}>Increment</button>
+        {/* we want to delete a counter, but since the counter component does not own the state of the counter, it only copys from the props, and since the component should
+        be deleted by the component that owns it, we need to raise en event from the child component that fires inside the parent component and delete it from there */}
+        <button className='btn btn-danger btn-sm m-2' onCLick={this.props.handleDelete(this.props.state.counter.id)}>Delete</button>
         <span style={this.styles}>Obj reference Stylying</span>
         {/* render inline style */}
         <span style={{color: 'blue', fontSize: '19px', textDecoration: 'underline'}}>Inline Stylying</span>
         {/* render styles dinamycally */}
         <span style={this.state.value > 0 ? this.green20 : this.red25}>Render styles dinamically</span>
         <button>click me</button>
-        <button className="btn btn-danger btn-sm m-2">Delete</button>
+        <button className="btn btn-danger btn-sm m-2" onClick={this.props.onDelete}>Delete</button>
       </>
     );
   }
