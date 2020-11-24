1839f6257c6be993c9d17575c669b35d67b10512
Andrea-Dispe <23516273+Andrea-Dispe@users.noreply.github.com>
2020-11-24 22:06:32 +0800

    Feat: refactored with one source of truth and implemented the handleIncrement as handler in the counters parent component

---
+++ src/components/Counters.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import Counter from "./Counter";
+
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 0, value: 50 },
+      { id: 1, value: 100 },
+      { id: 2, value: 150 },
+      { id: 3, value: 200 }
+    ],
+  };
+
+  handleDelete = (id) => {
+    console.log("id is ", id);
+    console.log("the state is ", this.state);
+    const counters = this.state.counters.filter((counter) => counter.id !== id);
+    // in the setState remeber to pass the new array inside an object
+    this.setState({ counters });
+  };
+  handleReset = () => {
+    const counters = this.state.counters.map((counter) => (counter.value = 0));
+    this.setState({ counters });
+  };
+
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const counterIndex = counters.indexOf(counter);
+    counters[counterIndex] = {...counter};
+    counters[counterIndex].value++;
+    this.setState({ counters })
+
+  };
+
+  render() {
+    return (
+      <>
+        <button
+          className="btn btn-primary btn-sm m-2"
+          onClick={this.handleReset}
+        >
+          Reset
+        </button>
+
+        {this.state.counters.map((counter) => (
+          <div key={counter.id} style={{ borderBottom: "3px solid black" }}>
+            <Counter
+              counter={counter}
+              onDelete={this.handleDelete}
+              onIncrement={this.handleIncrement}
+            />
+          </div>
+        ))}
+      </>
+    );
+  }
+}
+
+export default Counters;
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -18,14 +18,8 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let badges = "badge m-2 badge-";
-    badges += this.state.count === 0 ? 'warning' : 'primary';
+    badges += this.props.counter.value === 0 ? 'warning' : 'primary';
     return badges;
-  }
-
-  handleIncrement = () => {
-    // remember the setState takes an object
-    console.log('this.props from counter', this.props.value)
-    this.setState({value: this.state.value + 1})
   }
 
 
@@ -33,8 +27,8 @@ class Counter extends Component {
     return (
       <>
         <p>This is the #{this.props.counter.id} Counter</p>
-        <span>This State value {this.state.value}</span>
-        <button onClick={this.handleIncrement}>Increment</button>
+        <span>This State value {this.props.counter.value}</span>
+        <button onClick={() => this.props.onIncrement(this.props.counter)}>Increment</button>
         {/* we want to delete a counter, but since the counter component does not own the state of the counter, it only copys from the props, and since the component should
         be deleted by the component that owns it, we need to raise en event from the child component that fires inside the parent component and delete it from there */}
         <button className='btn btn-danger btn-sm m-2' onClick={() => this.props.onDelete(this.props.counter.id)}>Delete</button>
@@ -44,7 +38,7 @@ class Counter extends Component {
         {/* render the style by appending classes dynamically */}
         <span className={this.getBadgeClasses()}>This is dynamically rendered by appending bootstrap classes according to what the count is</span>
         {/* render styles dinamycally */}
-        <span style={this.state.value > 0 ? this.green20 : this.red25}>Render styles dinamically</span>
+        <span style={this.props.counter.value > 0 ? this.green20 : this.red25}>Render styles dinamically</span>
         <button>click me</button>
         {/* <button className="btn btn-danger btn-sm m-2" onClick={this.props.onDelete}>Delete</button> */}
       </>
