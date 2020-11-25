b36496431378114254d9bc7589d44d44f62c2514
Andrea-Dispe <23516273+Andrea-Dispe@users.noreply.github.com>
2020-11-25 11:14:04 +0800

    Fea: turned the Navigation component to a stateless functional component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,22 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Navigation extends Component {
-  render() {
-    return (
-      <>
-        <nav className="navbar navbar-light bg-light">
-          <a className="navbar-brand" href="#">
-            Navbar
-            <div>
-              <p className="badge badge-pill badge-secondary">
-                There are {this.props.counters.length} Counters
-              </p>
-            </div>
-          </a>
-        </nav>
-      </>
-    );
-  }
+const NavBar = (props) => {
+  return (
+    <>
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Navbar
+          <div>
+            <p className="badge badge-pill badge-secondary">
+              There are {props.counters.length} Counters
+            </p>
+          </div>
+        </a>
+      </nav>
+    </>
+  );
 }
 
-export default Navigation;
+export default NavBar;
+
+
